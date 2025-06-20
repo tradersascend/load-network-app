030c43132959010f6d9e8548e8ffd3b73edba70b
tradersascend <tradersascend@gmail.com>
2025-06-21 01:28:28 +0200

    Fixing server logic and adding new features

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/users/';
+const API_URL = 'https://load-network-api.onrender.com/api/users/';
 
 const getAuthToken = () => {
     const token = localStorage.getItem('token');
