49b09fb482cbf0716bbbe36d540c599307302c53
tradersascend <tradersascend@gmail.com>
2025-06-21 17:18:08 +0200

    Trigger fresh Vercel buildz6

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://load-network-api.onrender.com';
-const API_URL = `${API_BASE_URL}/api/users/`;
+const API_URL = '/api/users/';
 
 // For an admin to create a new user
 const createUser = (userData) => {
