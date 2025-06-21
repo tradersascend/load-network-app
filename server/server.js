1614ad208d8f60f09a6a0f74d4e74bdc4af366de
tradersascend <tradersascend@gmail.com>
2025-06-21 13:40:25 +0200

    Fixing server logic and adding new features

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const corsOptions = {
   credentials: true,
   optionsSuccessStatus: 200,
   //methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  //allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
